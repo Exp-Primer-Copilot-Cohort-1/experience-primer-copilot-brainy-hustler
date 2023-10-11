096e6236a5e5dcc132d0bf2a4022574359474ef2
brainy-hustler <147023876+brainy-hustler@users.noreply.github.com>
2023-10-11 19:58:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "blue";
+    member.style.fontSize = "20px";
+    member.style.fontWeight = "bold";
+    member.style.fontFamily = "Arial";
+    member.style.textAlign = "center";
+    member.style.backgroundColor = "yellow";
+    member.style.padding = "10px";
+    member.style.border = "1px solid black";
+    member.style.borderRadius = "5px";
+    member.style.width = "100px";
+    member.style.height = "50px";
+    member.style.margin = "auto";
+    member.style.marginTop = "5px";
+    member.style.marginBottom = "5px";
+    member.style.display = "block";
+    member.style.cursor = "pointer";
+    member.style.boxShadow = "2px 2px 2px 2px black";
+    member.style.transition = "all 1s";
+}
